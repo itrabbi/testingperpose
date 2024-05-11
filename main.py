2ef4b91a9f308e2a819650790ba3a047375f59b9
Rabbi Sudo <rabbisudo@gmail.com>
2024-05-11 23:19:31 +0600

    Test

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hello world!")
+print("Welcome to the programming world...")
+a = "Rabbi"
+b = "Sudo"
+print(a + " " + b)
+firstName, lastName = "Rabbi", "Hassan"
+print(firstName + " " + lastName)
